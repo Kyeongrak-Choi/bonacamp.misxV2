9281267d262186c0a0080f32d7732526360b01e6
howtominimal <dev.howtominimal@gmail.com>
2022-09-04 19:07:45 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:renew_misx/assets/translations/strings.dart';
-import 'package:renew_misx/screens/login.dart';
+import 'package:renew_misx/layouts/login.dart';
 
 void main() {
   runApp(Misx());
