84abf638b6e9e997898e907a247ec7c940990cbf
bona.kyeongrak <bona.kyeongrak@gmail.com>
2022-09-08 11:30:13 +0900

    commit

--- lib/components/common/datepicker/option_period_picker.dart
+++ lib/components/common/datepicker/option_period_picker.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:intl/intl.dart';
+import 'package:omni_datetime_picker/omni_datetime_picker.dart';
 import 'dart:developer';
 
 import 'package:renew_misx/constants.dart';
@@ -16,7 +17,8 @@ class OptionPeriodPicker extends StatelessWidget {
       children: [
         Obx(
           () => TextButton(
-            onPressed: () => Get.find<PeriodPickerController>().chooseFromDate(),
+            onPressed: () =>
+                Get.find<PeriodPickerController>().chooseFromDate(),
             child: Text(
               DateFormat('yyyy-MM-dd')
                   .format(Get.find<PeriodPickerController>().fromDate.value)
@@ -46,7 +48,6 @@ class PeriodPickerController extends GetxController {
   var pickedFromDate;
   var pickedToDate;
 
-
   @override
   void onInit() {
     super.onInit();
@@ -63,35 +64,60 @@ class PeriodPickerController extends GetxController {
   }
 
   chooseFromDate() async {
-    pickedFromDate = await showDatePicker(
+    // pickedFromDate = await showDatePicker(
+    //   context: Get.context!,
+    //   initialDate: fromDate.value,
+    //   firstDate: DateTime(DateTime.now().year - 3),
+    //   lastDate: DateTime(DateTime.now().year + 3),
+    //   //initialEntryMode: DatePickerEntryMode.input,
+    //   cancelText: 'cancel'.tr,
+    //   helpText: '',
+    //   builder: (context, child) {
+    //     return Theme(
+    //       data: Theme.of(context).copyWith(
+    //         colorScheme: ColorScheme.dark(
+    //           primary: Color(AppColor), // header background color
+    //           onPrimary: Colors.black, // header text color
+    //           onSurface: Colors.green, // body text color
+    //           background: Color(AppColor),
+    //           onBackground: Color(AppColor),
+    //
+    //         ),
+    //         textButtonTheme: TextButtonThemeData(
+    //           style: TextButton.styleFrom(
+    //             primary: Color(AppColor), // button text color
+    //           ),
+    //         ),
+    //       ),
+    //       child: child!,
+    //     );
+    //   },
+    // );
+
+    pickedFromDate = await showOmniDateTimePicker(
       context: Get.context!,
-      initialDate: fromDate.value,
-      firstDate: DateTime(DateTime.now().year - 3),
-      lastDate: DateTime(DateTime.now().year + 3),
-      //initialEntryMode: DatePickerEntryMode.input,
-      cancelText: 'cancel'.tr,
-      helpText: null,
-      builder: (context, child) {
-        return Theme(
-          data: Theme.of(context).copyWith(
-            colorScheme: ColorScheme.dark(
-              primary: Colors.yellow, // header background color
-              onPrimary: Colors.black, // header text color
-              onSurface: Colors.green, // body text color
-            ),
-            textButtonTheme: TextButtonThemeData(
-              style: TextButton.styleFrom(
-                primary: Color(AppColor), // button text color
-              ),
-            ),
-          ),
-          child: child!,
-        );
-      },
+      primaryColor: Colors.cyan,
+      backgroundColor: Colors.grey[900],
+      calendarTextColor: Colors.white,
+      tabTextColor: Colors.white,
+      unselectedTabBackgroundColor: Colors.grey[700],
+      buttonTextColor: Colors.white,
+      timeSpinnerTextStyle:
+          const TextStyle(color: Colors.white70, fontSize: 18),
+      timeSpinnerHighlightedTextStyle:
+          const TextStyle(color: Colors.white, fontSize: 24),
+      is24HourMode: false,
+      isShowSeconds: false,
+      startInitialDate: DateTime.now(),
+      startFirstDate: DateTime(1600).subtract(const Duration(days: 3652)),
+      startLastDate: DateTime.now().add(
+        const Duration(days: 3652),
+      ),
+      borderRadius: const Radius.circular(16),
     );
 
     if (pickedFromDate != null && pickedFromDate != fromDate.value) {
-      if(Validate()) {
+      if (Validate()) {
         fromDate.value = pickedFromDate;
       }
     }
@@ -102,14 +128,13 @@ class PeriodPickerController extends GetxController {
       context: Get.context!,
       initialDate: toDate.value,
       firstDate: DateTime(DateTime.now().year - 3),
-      lastDate: DateTime(DateTime.now().year + 3),
+      lastDate: DateTime(DateTime.now().year),
       //initialEntryMode: DatePickerEntryMode.input,
       cancelText: 'cancel'.tr,
-
     );
 
     if (pickedToDate != null && pickedToDate != toDate.value) {
-      if(Validate()) {
+      if (Validate()) {
         toDate.value = pickedToDate;
       }
     }
@@ -128,7 +153,7 @@ class PeriodPickerController extends GetxController {
         reverseAnimationCurve: Curves.easeOut,
       );
       return false;
-    }else {
+    } else {
       return true;
     }
   }
