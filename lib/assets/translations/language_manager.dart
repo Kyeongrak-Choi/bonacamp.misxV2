9ce878277e34afe72a6c49062df6d3182d286653
bona.kyeongrak <bona.kyeongrak@gmail.com>
2022-10-21 17:46:02 +0900

    commit

--- lib/assets/translations/language_manager.dart
+++ lib/assets/translations/language_manager.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'ko.dart';
 
 // multi language set
-class Strings extends Translations {
+class LanguageManager extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
         'ko_KR': koKR,
