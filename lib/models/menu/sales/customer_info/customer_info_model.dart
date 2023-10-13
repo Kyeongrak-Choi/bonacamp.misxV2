55001e4836c3086fba8be29da81d2f59a7cf1b61
bonaSWHeo <bona.swheo@gmail.com>
2023-10-13 15:33:10 +0900

    MISX2-27 / model 재 commit

--- lib/models/menu/sales/customer_info/customer_info_model.dart
+++ lib/models/menu/sales/customer_info/customer_info_model.dart
@@ -1,118 +1,80 @@
 import 'package:intl/intl.dart';
 import 'package:json_annotation/json_annotation.dart';
 
+import 'customer_info_employee_model.dart';
+import 'customer_info_representative_model.dart';
+import 'customer_info_sales_model.dart';
+
 part 'customer_info_model.g.dart'; // *.g.dart : in same directory
 
 // token response model
 @JsonSerializable(explicitToJson: true)
 class CustomerInfoModel {
-  String? ranking;
-  String? name;
-  String? phone;
-  String? birthDay;
-  String? address;
-  String? salesRep;
-  String? manager;
-  String? email;
+  CustomerInfoRepresentativeModel customerInfoRepresentativeModel;
+  CustomerInfoEmployeeModel customerInfoEmployeeModel;
+  CustomerInfoSalesModel customerInfoSalesModel;
+  String? businessNo;
   String? tel1;
   String? tel2;
   String? fax;
   String? note;
   String? address;
-  String? title;
-  String? month;
 
 
-  SalesRankModel(this.ranking, this.customerCode, this.customerName, this.salesAmount, this.supplementAmount, this.profitAmount, this.profitRate,
-      this.bondBalance, this.totalAmount,
-      {this.id});
+  CustomerInfoModel(this.customerInfoRepresentativeModel, this.customerInfoEmployeeModel, this.customerInfoSalesModel,
+                    this.businessNo, this.tel1, this.tel2, this.fax, this.note, this.address);
 
   Map<String, dynamic> toMap() {
     return {
-      'ranking': ranking,
-      'customer-code': customerCode,
-      'customer-name': customerName,
-      'sales-amount': salesAmount,
-      'supplement-amount': supplementAmount,
-      'profit-amount': profitAmount,
-      'profit-rate': profitRate,
-      'bond-balance': bondBalance,
-      'total-amount': totalAmount,
+      'representative' : customerInfoRepresentativeModel,
+      'employee' : customerInfoEmployeeModel,
+      'sales-summaries' : customerInfoSalesModel,
+      'business-no': businessNo,
+      'tel1': tel1,
+      'tel2': tel2,
+      'fax': fax,
+      'note': note,
+      'address': address,
     };
   }
 
-  factory SalesRankModel.fromJson(Map<String, dynamic> json) => _$SalesRankModelFromJson(json);
+  factory CustomerInfoModel.fromJson(Map<String, dynamic> json) => _$CustomerInfoModelFromJson(json);
 
-  Map<String, dynamic> toJson() => _$SalesRankModelToJson(this);
+  Map<String, dynamic> toJson() => _$CustomerInfoModelToJson(this);
 
-  String? get getRanking => ranking;
+  String? get getBusinessNo => businessNo;
 
-  set setRanking(String value) {
-    ranking = value;
+  set setBusinessNo(String value) {
+    businessNo = value;
   }
 
-  String? get getCustomerCode => customerCode;
+  String? get getTel1 => tel1;
 
-  set setCustomerCode(String value) {
-    customerCode = value;
+  set setTel1(String value) {
+    tel1 = value;
   }
 
-  String? get getCustomerName => customerName;
+  String? get getTel2 => tel2;
 
-  set setCustomerName(String value) {
-    customerName = value;
+  set setTel2(String value) {
+    tel2 = value;
   }
 
-  String? get getSalesAmount => salesAmount;
+  String? get getFax => fax;
 
-  set setSalesAmount(String value) {
-    salesAmount = value;
+  set setFax(String value) {
+    fax = value;
   }
 
-  String? get getSupplementAmount => supplementAmount;
+  String? get getNote => note;
 
-  set setSupplementAmount(String value) {
-    supplementAmount = value;
+  set setNote(String value) {
+    note = value;
   }
 
-  String? get getProfitAmount => profitAmount;
+  String? get getAddress => address;
 
-  set setProfitAmount(String value) {
-    profitAmount = value;
+  set setAddress(String value) {
+    address = value;
   }
-
-  String? get getProfitRate => profitRate;
-
-  set setProfitRate(String value) {
-    profitRate = value;
-  }
-
-  String? get getBondBalance => bondBalance;
-
-  set setBondBalance(String value) {
-    bondBalance = value;
-  }
-
-  String? get getTotalAmount => totalAmount;
-
-  set setTotalAmount(String value) {
-    totalAmount = value;
-  }
-}
-
-List<SalesRankModel> generateSalesRankList(dataList, count) {
-  var f = NumberFormat('###,###,###,###');
-  return List.generate(
-      count,
-          (index) => SalesRankModel(
-          dataList[index].ranking,
-          dataList[index].customerCode,
-          dataList[index].customerName,
-          f.format(int.parse(dataList[index].salesAmount ?? '0')),
-          f.format(int.parse(dataList[index].supplementAmount ?? '0')),
-          f.format(int.parse(dataList[index].profitAmount ?? '0')),
-          dataList[index].profitRate,
-          f.format(int.parse(dataList[index].bondBalance ?? '0')),
-          f.format(int.parse(dataList[index].totalAmount ?? '0')),
-          id: index));
 }
