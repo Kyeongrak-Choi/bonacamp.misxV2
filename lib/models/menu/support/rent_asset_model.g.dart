13cdbdfa03b28e19a5b212abe5b042664cd3e779
bona.kyeongrak <bona.kyeongrak@gmail.com>
2023-11-13 14:42:25 +0900

    Create rent_asset_model.g.dart

---
+++ lib/models/menu/support/rent_asset_model.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'rent_asset_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+RentAssetModel _$RentAssetModelFromJson(Map<String, dynamic> json) =>
+    RentAssetModel(
+      json['date'],
+      json['code'],
+      json['name'],
+      json['amount'],
+      json['type'],
+      json['serialNo'],
+      json['memo'],
+    );
+
+Map<String, dynamic> _$RentAssetModelToJson(RentAssetModel instance) =>
+    <String, dynamic>{
+      'date': instance.date,
+      'code': instance.code,
+      'name': instance.name,
+      'amount': instance.amount,
+      'type': instance.type,
+      'serialNo': instance.serialNo,
+      'memo': instance.memo,
+    };
