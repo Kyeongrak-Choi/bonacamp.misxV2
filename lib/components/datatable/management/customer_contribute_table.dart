f8f7f14fc6205112212f33275b7e4c686faf15c9
bonaSWHeo <bona.swheo@gmail.com>
2024-01-08 15:59:07 +0900

    매출처별 기여현황 디자인 적용. 종합현황 수정.

--- lib/components/datatable/management/customer_contribute_table.dart
+++ lib/components/datatable/management/customer_contribute_table.dart
@@ -19,36 +19,36 @@ class CustomerContributeTable extends StatelessWidget {
             children: [
               IconTitleField(
                 titleName: '매출공급가',
-                value: controller.controllerCustomerContribute != null
+                value: (controller.controllerCustomerContribute != null
                     ? numberFormat.format(controller
                             .controllerCustomerContribute.supplementAmount ??
                         '0')
-                    : '0',
+                    : '0') + ' 원',
                 iconData: Icons.label_outlined,
               ),
               IconTitleField(
                 titleName: '매출원가',
-                value: controller.controllerCustomerContribute != null
+                value: (controller.controllerCustomerContribute != null
                     ? numberFormat.format(controller
                             .controllerCustomerContribute.purchaseAmount ??
                         '0')
-                    : '0',
+                    : '0') + ' 원',
                 iconData: Icons.label_outlined,
               ),
               IconTitleField(
                 titleName: '마진율',
-                value: controller.controllerCustomerContribute != null
+                value: (controller.controllerCustomerContribute != null
                     ? controller.controllerCustomerContribute.profitRate
                         .toString()
-                    : '0',
+                    : '0') + ' %',
                 iconData: Icons.label_outlined,
               ),
               IconTitleField(
                 titleName: '매출이익',
-                value: controller.controllerCustomerContribute != null
+                value: (controller.controllerCustomerContribute != null
                     ? numberFormat.format(
                         controller.controllerCustomerContribute.profitAmount)
-                    : '0',
+                    : '0') + ' 원',
                 iconData: Icons.label_outlined,
               ),
               Container(
@@ -58,34 +58,34 @@ class CustomerContributeTable extends StatelessWidget {
                       color: context.theme.primaryColor, thickness: 0.5)),
               IconTitleField(
                 titleName: '관리비용',
-                value: controller.controllerCustomerContribute != null
+                value: (controller.controllerCustomerContribute != null
                     ? numberFormat.format(controller
                         .controllerCustomerContribute.managementAmount)
-                    : '0',
+                    : '0') + ' 원',
                 iconData: Icons.label_outlined,
               ),
               IconTitleField(
                 titleName: '금융비용',
-                value: controller.controllerCustomerContribute != null
+                value: (controller.controllerCustomerContribute != null
                     ? numberFormat.format(
                         controller.controllerCustomerContribute.financeAmount)
-                    : '0',
+                    : '0') + ' 원',
                 iconData: Icons.label_outlined,
               ),
               IconTitleField(
                 titleName: '자산수리비',
-                value: controller.controllerCustomerContribute != null
+                value: (controller.controllerCustomerContribute != null
                     ? numberFormat.format(
                         controller.controllerCustomerContribute.assetFixAmount)
-                    : '0',
+                    : '0') + ' 원',
                 iconData: Icons.label_outlined,
               ),
               IconTitleField(
                 titleName: '비용합계',
-                value: controller.controllerCustomerContribute != null
+                value: (controller.controllerCustomerContribute != null
                     ? numberFormat
                         .format(controller.controllerCustomerContribute.total)
-                    : '0',
+                    : '0') + ' 원',
                 iconData: Icons.label_outlined,
               ),
               Container(
@@ -95,26 +95,26 @@ class CustomerContributeTable extends StatelessWidget {
                       color: context.theme.primaryColor, thickness: 0.5)),
               IconTitleField(
                 titleName: '채권잔액',
-                value: controller.controllerCustomerContribute != null
+                value: (controller.controllerCustomerContribute != null
                     ? numberFormat.format(
                         controller.controllerCustomerContribute.bondBalance)
-                    : '0',
+                    : '0') + ' 원',
                 iconData: Icons.label_outlined,
               ),
               IconTitleField(
                 titleName: '대여금액',
-                value: controller.controllerCustomerContribute != null
+                value: (controller.controllerCustomerContribute != null
                     ? numberFormat.format(
                         controller.controllerCustomerContribute.rentalBalance)
-                    : '0',
+                    : '0') + ' 원',
                 iconData: Icons.label_outlined,
               ),
               IconTitleField(
                 titleName: '대여자산',
-                value: controller.controllerCustomerContribute != null
+                value: (controller.controllerCustomerContribute != null
                     ? numberFormat.format(
                         controller.controllerCustomerContribute.rentalAmount)
-                    : '0',
+                    : '0') + ' 원',
                 iconData: Icons.label_outlined,
               ),
               IconTitleField(
@@ -129,10 +129,10 @@ class CustomerContributeTable extends StatelessWidget {
               ),
               IconTitleField(
                 titleName: '기여금액',
-                value: controller.controllerCustomerContribute != null
+                value: (controller.controllerCustomerContribute != null
                     ? numberFormat.format(controller
                         .controllerCustomerContribute.contributeAmount)
-                    : '0',
+                    : '0') + ' 원',
                 iconData: Icons.label_outlined,
               ),
               Container(
