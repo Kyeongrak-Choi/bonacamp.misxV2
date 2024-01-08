b74878a2775d33fa6894dc02aefabc3b6b7d0ef2
bona.kyeongrak <bona.kyeongrak@gmail.com>
2024-01-08 15:56:20 +0900

    재고현황 디자인 적용

--- lib/components/datatable/inventory/inventory_report_item.dart
+++ lib/components/datatable/inventory/inventory_report_item.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import '../../../models/menu/inventory/inventory_report_model.dart';
+import '../../../utils/constants.dart';
 import '../../../utils/theme/color_manager.dart';
 import '../../common/field/icon_title_field.dart';
 
@@ -15,209 +17,231 @@ class InventoryReportItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: context.theme.cardColor,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-          backgroundColor: context.theme.cardColor,
-          padding: EdgeInsetsDirectional.fromSTEB(0, 5, 5, 0),
+          backgroundColor: context.theme.canvasColor,
         ),
         onPressed: () {},
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 15),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 4,
-                    child: Text(
-                      '품목',
-                      style: context.textTheme.displayMedium,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Text(
-                      'BOX',
-                      style: context.textTheme.displayMedium,
-                      textAlign: TextAlign.right,
-                    ),
-                  ),
-                  Expanded(
-                    flex: 2,
-                    child: Text(
-                      'EA',
-                      style: context.textTheme.displayMedium,
-                      textAlign: TextAlign.right,
-                    ),
-                  ),
-                  Expanded(
-                    flex: 3,
-                    child: Text(
-                      '금액',
-                      style: context.textTheme.displayMedium,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Text(
-                      '상세',
-                      style: context.textTheme.displayMedium,
-                      textAlign: TextAlign.center,
-                    ),
-                  )
-                ],
-              ),
-            ),
-            ListView.separated(
-              primary: false,
-              shrinkWrap: true,
-              //padding: const EdgeInsets.all(10),
-              itemCount: dataList.length,
-              // Divider 로 구분자 추가.
-              separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(
-                height: 5,
-                color: CommonColors.black,
-              ),
-              itemBuilder: (BuildContext context, int index) {
-                return Row(
+        child: Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(0.w, BASIC_PADDING.h, 0.w, BASIC_PADDING.h),
+          child: Column(
+            children: [
+              Container(
+                child: Row(
                   children: [
                     Expanded(
                       flex: 4,
                       child: Text(
-                        dataList[index].itemName ?? '',
-                        style: context.textTheme.displaySmall,
-                        textAlign: TextAlign.left,
+                        '품목',
+                        style: context.textTheme.bodyMedium,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
                       ),
                     ),
                     Expanded(
                       flex: 2,
                       child: Text(
-                        dataList[index].boxQuantity,
+                        'BOX',
                         style: context.textTheme.bodyMedium,
                         textAlign: TextAlign.right,
+                        overflow: TextOverflow.ellipsis,
                       ),
                     ),
                     Expanded(
                       flex: 2,
                       child: Text(
-                        dataList[index].bottleQuantity,
+                        'EA',
                         style: context.textTheme.bodyMedium,
                         textAlign: TextAlign.right,
+                        overflow: TextOverflow.ellipsis,
                       ),
                     ),
                     Expanded(
                       flex: 3,
                       child: Text(
-                        dataList[index].amount,
+                        '금액',
                         style: context.textTheme.bodyMedium,
-                        textAlign: TextAlign.right,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
                       ),
                     ),
                     Expanded(
                       flex: 1,
-                      child: IconButton(
-                        onPressed: () {
-                          ShowInventoryDetailDialog(dataList[index], context);
-                        },
-                        icon: Icon(Icons.search,
-                            color: context.theme.primaryColor),
+                      child: Text(
+                        '상세',
+                        style: context.textTheme.bodyMedium,
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
                       ),
                     )
                   ],
-                );
-              },
-            ),
-          ],
+                ),
+              ),
+              ListView.separated(
+                primary: false,
+                shrinkWrap: true,
+                itemCount: dataList.length,
+                // Divider 로 구분자 추가.
+                separatorBuilder: (BuildContext context, int index) => const Divider(
+                  height: 0,
+                  color: CommonColors.gray,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  return Row(
+                    children: [
+                      Expanded(
+                        flex: 4,
+                        child: Text(
+                          dataList[index].itemName ?? '',
+                          style: context.textTheme.bodyMedium,
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          dataList[index].boxQuantity,
+                          style: context.textTheme.bodyMedium,
+                          textAlign: TextAlign.right,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 2,
+                        child: Text(
+                          dataList[index].bottleQuantity,
+                          style: context.textTheme.bodyMedium,
+                          textAlign: TextAlign.right,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 3,
+                        child: Text(
+                          dataList[index].amount,
+                          style: context.textTheme.bodyMedium,
+                          textAlign: TextAlign.right,
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: IconButton(
+                          onPressed: () {
+                            ShowInventoryDetailDialog(dataList[index], context);
+                          },
+                          icon: Icon(
+                            Icons.search,
+                            size: 24.sp,
+                          ),
+                        ),
+                      )
+                    ],
+                  );
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
 
-void ShowInventoryDetailDialog(var detailList, context) {
+void ShowInventoryDetailDialog(var detailList, BuildContext context) {
   Get.defaultDialog(
-      title: "\n재고 현황 상세보기",
-      titleStyle: TextStyle(color: CommonColors.primary),
+      title: '',
+      backgroundColor: context.theme.canvasColor,
       content: Container(
         height: MediaQuery.of(context).size.height * 0.6,
-        width: MediaQuery.of(context).size.width * 0.85,
-        child: ListView(
-          children: [
-            IconTitleField(
-              titleName: '품목',
-              value: detailList.itemName ?? '',
-              iconData: Icons.label_outlined,
-            ),
-            IconTitleField(
-              titleName: 'BOX',
-              value: detailList.boxQuantity,
-              iconData: Icons.label_outlined,
-            ),
-            IconTitleField(
-              titleName: 'EA',
-              value: detailList.bottleQuantity,
-              iconData: Icons.label_outlined,
-            ),
-            IconTitleField(
-              titleName: '금액',
-              value: detailList.amount,
-              iconData: Icons.label_outlined,
-            ),
-            IconTitleField(
-              titleName: '매입처',
-              value: detailList.customerName ?? '',
-              iconData: Icons.label_outlined,
-            ),
-            IconTitleField(
-              titleName: '매입단가(BOX)',
-              value: detailList.purchase.box,
-              iconData: Icons.label_outlined,
-            ),
-            IconTitleField(
-              titleName: '매입단가(EA)',
-              value: detailList.purchase.bottle,
-              iconData: Icons.label_outlined,
-            ),
-            IconTitleField(
-              titleName: '매출단가(BOX)',
-              value: detailList.sales.box,
-              iconData: Icons.label_outlined,
-            ),
-            IconTitleField(
-              titleName: '매출단가(EA)',
-              value: detailList.sales.bottle,
-              iconData: Icons.label_outlined,
-            ),
-            IconTitleField(
-              titleName: '입수량',
-              value: detailList.packageQuantity,
-              iconData: Icons.label_outlined,
-            ),
-            IconTitleField(
-              titleName: '주종',
-              value: detailList.liquorType ?? '',
-              iconData: Icons.label_outlined,
-            ),
-            IconTitleField(
-              titleName: '판매분류',
-              value: detailList.salesType ?? '',
-              iconData: Icons.label_outlined,
-            ),
-            IconTitleField(
-              titleName: '품목분류',
-              value: detailList.itemType ?? '',
-              iconData: Icons.label_outlined,
-            ),
-            IconTitleField(
-              titleName: '용도',
-              value: detailList.use ?? '',
-              iconData: Icons.label_outlined,
-            ),
-          ],
+        width: MediaQuery.of(context).size.width * 0.8,
+        child: Padding(
+          padding: EdgeInsetsDirectional.fromSTEB(BASIC_PADDING.w, 0.h, BASIC_PADDING.w, BASIC_PADDING.h),
+          child: ListView(
+            children: [
+              Text(
+                '재고현황 상세보기',
+                style: context.textTheme.titleSmall?.copyWith(
+                  fontWeight: FontWeight.bold,
+                  color: context.theme.colorScheme.onPrimary,
+                ),
+              ),
+              SizedBox(
+                height: BASIC_PADDING.h,
+              ),
+              Divider(
+                height: 1.h,
+                color: context.theme.colorScheme.onPrimary,
+              ),
+              IconTitleField(
+                titleName: '품목',
+                value: detailList.itemName ?? '',
+                iconData: Icons.label_outlined,
+              ),
+              IconTitleField(
+                titleName: 'BOX',
+                value: detailList.boxQuantity,
+                iconData: Icons.label_outlined,
+              ),
+              IconTitleField(
+                titleName: 'EA',
+                value: detailList.bottleQuantity,
+                iconData: Icons.label_outlined,
+              ),
+              IconTitleField(
+                titleName: '금액',
+                value: detailList.amount,
+                iconData: Icons.label_outlined,
+              ),
+              IconTitleField(
+                titleName: '매입처',
+                value: detailList.customerName ?? '',
+                iconData: Icons.label_outlined,
+              ),
+              IconTitleField(
+                titleName: '매입단가(BOX)',
+                value: detailList.purchase.box,
+                iconData: Icons.label_outlined,
+              ),
+              IconTitleField(
+                titleName: '매입단가(EA)',
+                value: detailList.purchase.bottle,
+                iconData: Icons.label_outlined,
+              ),
+              IconTitleField(
+                titleName: '매출단가(BOX)',
+                value: detailList.sales.box,
+                iconData: Icons.label_outlined,
+              ),
+              IconTitleField(
+                titleName: '매출단가(EA)',
+                value: detailList.sales.bottle,
+                iconData: Icons.label_outlined,
+              ),
+              IconTitleField(
+                titleName: '입수량',
+                value: detailList.packageQuantity,
+                iconData: Icons.label_outlined,
+              ),
+              IconTitleField(
+                titleName: '주종',
+                value: detailList.liquorType ?? '',
+                iconData: Icons.label_outlined,
+              ),
+              IconTitleField(
+                titleName: '판매분류',
+                value: detailList.salesType ?? '',
+                iconData: Icons.label_outlined,
+              ),
+              IconTitleField(
+                titleName: '품목분류',
+                value: detailList.itemType ?? '',
+                iconData: Icons.label_outlined,
+              ),
+              IconTitleField(
+                titleName: '용도',
+                value: detailList.use ?? '',
+                iconData: Icons.label_outlined,
+              ),
+            ],
+          ),
         ),
       ));
 }
