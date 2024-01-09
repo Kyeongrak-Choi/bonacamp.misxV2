8ce6d51f521ca77cc5cd6bffc94e7719d142c8bd
bonaSWHeo <bona.swheo@gmail.com>
2024-01-09 09:28:56 +0900

    매출 순위현황 디자인 적용

--- lib/layouts/menu/management/sales_rank.dart
+++ lib/layouts/menu/management/sales_rank.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:hive/hive.dart';
 import 'package:intl/intl.dart';
@@ -29,62 +30,57 @@ class SalesRank extends StatelessWidget {
     return Obx(() => Scaffold(
           appBar: AppBar(title: Text('menu_sub_sales_rank'.tr), actions: []),
           body: Container(
-            color: context.theme.canvasColor,
+            color: context.theme.colorScheme.background,
             child: Stack(
               children: [
-                Padding(
-                  padding: EdgeInsetsDirectional.all(15),
-                  child: Column(
-                    children: [
-                      Visibility(
-                        visible: Get.find<SalesRankController>().visible.value,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: context.theme.cardColor,
-                            borderRadius: BorderRadius.circular(15),
-                            shape: BoxShape.rectangle,
-                          ),
-                          child: Padding(
-                            padding: EdgeInsetsDirectional.all(15),
-                            child: Column(
-                              children: [
-                                OptionPeriodPicker(),
-                                OptionCbBranch(),
-                                OptionTwoContent(
-                                    OptionCbEmployee(), OptionCbManager()),
-                                OptionBtnSearch(ROUTE_MENU_RANKSTATUS),
-                              ],
-                            ),
+                Column(
+                  children: [
+                    Visibility(
+                      visible: Get.find<SalesRankController>().visible.value,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: context.theme.cardColor,
+                        ),
+                        child: Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(
+                              BASIC_PADDING * 2.w,
+                              BASIC_PADDING * 2.h,
+                              BASIC_PADDING * 2.w,
+                              BASIC_PADDING * 2.h),
+                          child: Column(
+                            children: [
+                              OptionPeriodPicker(),
+                              OptionCbBranch(),
+                              OptionTwoContent(
+                                  OptionCbEmployee(), OptionCbManager()),
+                              OptionBtnSearch(ROUTE_MENU_RANKSTATUS),
+                            ],
                           ),
                         ),
                       ),
-                      SizedBox(
-                        height: Get.find<SalesRankController>().visible.value
-                            ? 20
-                            : 0,
-                      ),
-                      Expanded(
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: context.theme.cardColor,
-                            borderRadius: BorderRadius.circular(15),
-                            shape: BoxShape.rectangle,
-                          ),
-                          child: Padding(
-                            padding: EdgeInsetsDirectional.all(15),
-                            child: ListView(
-                              children: <Widget>[setChild()],
-                            ),
-                          ),
+                    ),
+                    SizedBox(
+                      height: Get.find<SalesRankController>().visible.value
+                          ? BASIC_PADDING.h
+                          : 0,
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: ListView(
+                          children: <Widget>[setChild()],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
                 Align(
                   alignment: Alignment.topRight,
                   child: Padding(
-                    padding: const EdgeInsets.all(5),
+                    padding: EdgeInsetsDirectional.fromSTEB(
+                        0.w,
+                        0.h,
+                        BASIC_PADDING * 2.w,
+                        0.h),
                     child: FloatingActionButton.small(
                       child: OptionBtnVisible(
                           visible:
@@ -92,8 +88,7 @@ class SalesRank extends StatelessWidget {
                       onPressed: () {
                         Get.find<SalesRankController>().setVisible();
                       },
-                      splashColor: CommonColors.primary,
-                      backgroundColor: Colors.white,
+                      backgroundColor: context.theme.colorScheme.onTertiary,
                       elevation: 1,
                     ),
                   ),
